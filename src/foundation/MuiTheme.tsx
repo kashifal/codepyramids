e3524b3beea7c408e84ad6e5f7539419cc169d0a
kashif s <kashee@softpyramid.com>
2022-08-07 00:35:07 +0500

    registeration completed

---
+++ src/foundation/MuiTheme.tsx
@@ -0,0 +1,73 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#d6304c",
+    },
+    // secondary: {
+    //   main: '#fb8500',
+    // },
+    // success: {
+    //   main: '#8ac926',
+    // },
+    // info: {
+    //   main: '#00bbf9',
+    // },
+    // error: {
+    //   main: '#f44336',
+    // },
+    // background: {
+    //   default: '#fff',
+    // },
+    // grey: '#8d99ae',
+  },
+
+  // set default component styles
+  //   components: {
+  //     MuiGrid: {
+  //       styleOverrides: {
+  //         root: {
+  //           position: "static",
+  //         },
+  //       },
+  //     },
+  //     MuiButtonBase: {
+  //       styleOverrides: {
+  //         root: {
+  //           //position: 'static',
+  //         },
+  //       },
+  //     },
+  //     MuiAccordion: {
+  //       styleOverrides: {
+  //         root: {
+  //           position: "static",
+  //         },
+  //       },
+  //     },
+  //     MuiAccordionSummary: {
+  //       styleOverrides: {
+  //         root: {
+  //           position: "static",
+  //         },
+  //       },
+  //     },
+  //     MuiAccordionDetails: {
+  //       styleOverrides: {
+  //         root: {
+  //           position: "static",
+  //         },
+  //       },
+  //     },
+  //     MuiPaper: {
+  //       styleOverrides: {
+  //         root: {
+  //           //position: 'static !important',
+  //         },
+  //       },
+  //     },
+  //   },
+});
+
+export default theme;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
+import { ThemeProvider } from "@material-ui/core";
+import theme from "./foundation/MuiTheme";
 import App from "./App";
 import { store, persistor } from "./app/store/configureStore";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+  <ThemeProvider theme={theme}>
+    <Router>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </Router>
+  </ThemeProvider>
 );
