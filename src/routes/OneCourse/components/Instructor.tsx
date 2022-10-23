986837cfed846ac13da83016fccc28594b75418c
Kashif Sulehria <kashee@softpyamid.com>
2022-10-23 12:54:31 +0500

    more stuff

--- src/routes/OneCourse/components/Instructor.tsx
+++ src/routes/OneCourse/components/Instructor.tsx
@@ -16,12 +16,12 @@ const Instructor = () => {
             <h1 className="text-[24px] font-black text-custom-primary">
               Course Instructor
             </h1>
-            <div className="flex mt-4">
-              <div className="flex  items-center justify-center">
-                <div className="w-24 h-24 rounded-full object-contain overflow-hidden border-2  ">
+            <div className="flex mt-4 ">
+              <div className="flex bg-custom-primary p-4 items-center justify-center">
+                <div className="w-24 h-24 rounded-full object-contain overflow-hidden   ">
                   <img
                     className="object-contain"
-                    src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZmFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                    src="https://images.unsplash.com/photo-1544717305-2782549b5136?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dGVhY2hlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
                     alt=""
                   />
                 </div>
@@ -29,9 +29,9 @@ const Instructor = () => {
 
               <div className="ml-4">
                 <h3 className="text-xl font-bold mt-2 text-custom-primary">
-                   Kashif Sulehria <span className="text-xs font-thin text-amber-500">(Full stack developer)</span>
+                   Zara Fatma
                 </h3>
-
+                <span className="text-xs font-thin text-amber-500">(Full stack developer)</span>
                 <Stack direction="row"  className="mt-2 flex flex-wrap gap-2 items-start justify-start" >
                   <Chip className="text-custom-primary" label="React" />
                   <Chip className="text-custom-primary" label="HTML5" />
